707abbc5503380cef0727adcb003e0919f21e7bc
kush789 <singh.kushagra.1996@gmail.com>
2017-08-25 19:25:55 +0530

    Fixed unblurring order in extension, published version 1.7

--- Chrome_extension/unblur.js
+++ Chrome_extension/unblur.js
@@ -10,14 +10,19 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 function unblurDoc() {
-	
+
 	$('.autogen_class_views_read2_page_blur_promo').remove();
+
+	$('.page_blur_promo').remove();
+	$('.page_blur_promo_overlay').remove();
+
 	$('.between_page_ads').remove();
+
 	$('.text_layer').css('color', '#000');
 	$('.absimg').css('opacity', '1.0');
 	$('.text_layer').css('text-shadow','black 0px 0px 0px');
 	$('.page_blur_promo').remove(); // Removes pesky ads.
-
+	
 	setTimeout(unblurDoc, 1000);
 };
 
